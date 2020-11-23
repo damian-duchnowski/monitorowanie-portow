97de37f9aa1e6e2603297a9e7177c600672a9d2d
Damian Duchnowski <damianduchnowski1@gmail.com>
2020-11-23 18:24:54 +0100

    Poprawa jednostki interwału między skanami

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 from notify import send_emails
 from settings import SCAN_INTERVAL_MIN
 
-schedule.every(SCAN_INTERVAL_MIN).seconds.do(send_emails)
+schedule.every(SCAN_INTERVAL_MIN).minutes.do(send_emails)
 
 while True:
     schedule.run_pending()
